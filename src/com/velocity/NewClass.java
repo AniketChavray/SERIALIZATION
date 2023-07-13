27a502c7a71a3938773ff2cffc8ef0d91fe1b78b
Aniket Chavray <sincereseeker000@gmail.com>
2023-07-13 16:58:20 +0530

    New Class added

---
+++ src/com/velocity/NewClass.java
@@ -0,0 +1,9 @@
+package com.velocity;
+
+public class NewClass {
+
+	public static void main(String args[]) {
+		
+		System.out.println("New classes added for the purpose of importing and again exporting to git hub");
+	}
+}
